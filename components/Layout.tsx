6b5a28e324fef4be541486caafcc144b3101123f
Martin Rodin <rodin.martin@gmail.com>
2021-01-02 17:43:32 +0100

    fix ts errors

---
+++ components/Layout.tsx
@@ -0,0 +1,19 @@
+import Footer from "./footer";
+import Meta from "./meta";
+
+type Props = {
+    preview?: boolean;
+    children: React.ReactNode;
+};
+
+const Layout = ({ preview, children }: Props) => (
+    <>
+        <Meta />
+        <div className="min-h-screen">
+            <main>{children}</main>
+        </div>
+        <Footer />
+    </>
+);
+
+export default Layout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,17 @@
-import { FC } from "react";
-
 import Container from "../components/Container";
 import MoreStories from "../components/more-stories";
-import HeroPost from "../components/hero-post";
 import Intro from "../components/Intro";
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 import { getAllPosts } from "../lib/api";
 import Head from "next/head";
 
-const Index: FC = ({ allPosts }) => (
+import Post from "../types/post";
+
+type Props = {
+    allPosts: Post[];
+};
+
+const Index = ({ allPosts }: Props) => (
     <Layout>
         <Head>
             <title>Martin Rodin - Front-end Web Developer</title>
