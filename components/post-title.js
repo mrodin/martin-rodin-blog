3f2c50bc223d804ad75bdca4a93b72c8798e43f4
Martin Rodin <rodin.martin@gmail.com>
2022-11-01 19:50:59 +0100

    fix blog article head title

--- components/post-title.js
+++ components/post-title.js
@@ -1,5 +1,5 @@
 const PostTitle = ({ children }) => (
-  <h1 className="text-6xl md:text-7xl lg:text-7xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left gradient-title">
+  <h1 className="text-6xl md:text-7xl lg:text-7xl font-bold tracking-tighter leading-tight mb-12 text-center md:text-left gradient-title">
     {children}
   </h1>
 );
--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -15,6 +15,9 @@ export default function Post({ post, morePosts, preview }) {
   if (!router.isFallback && !post?.slug) {
     return <ErrorPage statusCode={404} />;
   }
+
+  const title = `${post.title} | Martin Rodin - Front-end Web Developer`;
+
   return (
     <Layout preview={preview}>
       <Container>
@@ -25,9 +28,7 @@ export default function Post({ post, morePosts, preview }) {
           <>
             <article className="mb-32">
               <Head>
-                <title>
-                  {post.title} | Martin Rodin - Front-end Web Developer
-                </title>
+                <title>{title}</title>
                 <meta property="og:image" content={post.ogImage.url} />
               </Head>
               <PostHeader
